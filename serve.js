3cd2f9cc5aad04521e8ab20f53364c3e9c52ab1a
Michael Tambornino <michael.tambornino@gmail.com>
2018-03-09 11:02:36 -0600

    Add graphql endpoint

--- serve.js
+++ serve.js
@@ -1,8 +1,14 @@
 const express = require('express')
-
+const graphqlHTTP = require('express-graphql')
 const app = express()
-
 const port = 4000
 
+const schema = require(./schema)
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+
 app.listen(port)
-console.log(`Listening on port {port}... `)
+console.log(`Listening on port ${port}... `)
